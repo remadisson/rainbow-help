117b13e0efa7577772aac2750e41c52984f42884
RainbowSpeedy <TheRealSpeedy@users.noreply.github.com>
2020-08-03 17:59:10 +0200

    Initial commit

---
+++ src/main/java/red/rainbow/help/Help.java
@@ -0,0 +1,31 @@
+package red.rainbow.help;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.command.Command;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.command.CommandManager;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "help",
+        name = "RainbowHelp",
+        version = "1.0",
+        description = "Provides /help command.",
+        url = "https://rainbow.red",
+        authors = {"RainbowSpeedy"}
+)
+public class Help {
+
+    @Inject
+    private Logger logger;
+
+    @Inject
+    private CommandManager cmdMgr;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        cmdMgr.register(new CmdExec(), "help");
+    }
+}
