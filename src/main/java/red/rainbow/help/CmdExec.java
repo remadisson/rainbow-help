6eadf5f6f7a78901568f3c72dc4b5cc7ee4047ca
Maximilian Loers <maximilianloers@icloud.com>
2020-08-04 14:19:14 +0200

    console-help-fix
    Extra If clause to execlude Console, from the loop

--- src/main/java/red/rainbow/help/CmdExec.java
+++ src/main/java/red/rainbow/help/CmdExec.java
@@ -12,13 +12,15 @@ public class CmdExec implements Command {
 
     @Override
     public void execute(CommandSource source, String[] args) {
-        if (args.length == 0) {
-            source.sendMessage(TextComponent.of(getMainscreen()));
-        } else if (args.length == 1) {
-            if (Arrays.stream(getCategories()).anyMatch(cat -> cat.equalsIgnoreCase(args[0]))) {
-                source.sendMessage(TextComponent.of(getScreenForCategory(args[0])));
-            } else {
+        if(sender instanceof Player) {
+            if (args.length == 0) {
                 source.sendMessage(TextComponent.of(getMainscreen()));
+            } else if (args.length == 1) {
+                if (Arrays.stream(getCategories()).anyMatch(cat -> cat.equalsIgnoreCase(args[0]))) {
+                    source.sendMessage(TextComponent.of(getScreenForCategory(args[0])));
+                } else {
+                    source.sendMessage(TextComponent.of(getMainscreen()));
+                }
             }
         }
     }
